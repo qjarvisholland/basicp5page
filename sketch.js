d36ae257364b907b90b14d02448132da2c1a27b4
qjarvisholland <quinnjarvisholland@gmail.com>
2019-01-21 22:15:34 -0800

    complete code overhaul, now a 2D shape inspired by the words swift and steep

--- sketch.js
+++ sketch.js
@@ -1,81 +1,68 @@
-let bubbles = [];
-var xoffset = 0.0;
+var custompointX;
+var custompointY;
+
+let customPtArrayX= [];
+let customPtArrayY= [];
 
 function setup() {
-	createCanvas(600,400);
+custompointX = 0;
+custompointY = 0;
+	createCanvas(400,400);
+	background(0);
 
-}
-
-function mousePressed() {
-//		for (let i = 0; i < 10; i++) {
-//	let x = random(width);
-	let r = random(10, 40);
-	let b = new Bubble(mouseX, mouseY, r);
-	bubbles.push(b);
-
-	//}
-}
-
-function keyPressed() {
-	if (keyCode = 'c') {
-		bubbles.pop();
-	}
-	xoffset + 0.01;
-
+stroke(255);
+noFill();
 }
 
 function draw() {
-
-
-	background(0);
-	fill(255);
-	text('click to add bubble object and press c to remove', 10, 10, width, 30)
-
-for (let bubble of bubbles) {
-	bubble.move();
-	bubble.show();
-}
-
-/*	for of loop above functions same as below
-for (let i = 0; i < bubbles.length; i++) {
-xoffset = xoffset + 0.01;
-	bubbles[i].move();
-	bubbles[i].show();
-	}
-*/
-
-
-}
-
-
-
-class Bubble {
-	constructor(x, y, r) {
-	this.x = x;
-	this.y = y;
-	this.r = r;
-	}
-
-	move() {
-	if (frameRate() % random(10, 30)) {
-		//xoffset = xoffset + 0.01;
-		xoffset = xoffset + random(0.01,1.0);
-	}
-	this.x = this.x + map(noise(xoffset+100), 0.0, 0.9, -6, 5.8);
 	
-	this.y = this.y + map(noise(xoffset), 0.0, 0.9, -6, 5.8);
+background(0);
+noFill();
+	point(mouseX, mouseY);
+		ellipse(customPtArrayX[2], 50, 40);
+	beginShape();
+	vertex(100,50);
+	vertex(200,20);
+	curveVertex(custompointX, custompointY);
+
+	curveVertex(200,300);
+	vertex(40,90);
+	endShape(CLOSE);
+
+fill(50);
+translate(width/2,height/2);
+beginShape();
+vertex(-27.46, 0.925);
+bezierVertex(-10.741, 30.767, 22.638, 65.443, 46.362, 86.044);
+bezierVertex(40.273, 49.07, 53.737, 103.694, 62.859, 109.777);
+bezierVertex(62.859, 109.777, 54.741, 129.886, -23.718, -21.525);
+bezierVertex(-40.916, -49.927, -35.218, -49.21, -56.457, -77.832);
+bezierVertex(-60.737, -89.943, -64.239, -114.837, -64.239, -114.837);
+bezierVertex(-72.106, -147.476, -38.932, -124.643, -31.382, -118.442);
+bezierVertex(-31.382, -118.442, -20.417, -114.956, -23.334, -89.185);
+bezierVertex(-5.762, 23.446, 33.937, 58.866, 33.937, 58.866);
+bezierVertex(38.494, 50.084, 13.178, 5.126, 28.485, 10.054);
+bezierVertex(37.898, 17.653, 70.782, 119.662, 62.859, 109.777);
+bezierVertex(48.799, 121.726, 26.664, 94.616, 6.795, 67.376);
+bezierVertex(-0.501, 59.375, -30.838, 19.038, -37.685, 10.355);
+bezierVertex(-38.723, 4.455, -32.308, -5.54, -27.46, 0.925);
+endShape();
+
+
 	}
 
-	// ISSUE; all bubbles move together randomly, i want them to individually pick places along noise
+function mousePressed() {
+   for (var i = 0; i <= 4; i++) {
+       if (i in customPtArrayX) {
+	customPtArrayX[i] = mouseX;
+	customPtArrayY[i] = mouseY;    s = myStringArray[i];
 
-	show() {
-	//stroke(50);
-	//strokeWeight(4);
-	//noFill();
-	noStroke();
-	fill(255,30);
-	ellipse(this.x, this.y, this.r * 3);
+  }
+   
+
+
+	
+	
 	}
-
+	i++;
 }
-
